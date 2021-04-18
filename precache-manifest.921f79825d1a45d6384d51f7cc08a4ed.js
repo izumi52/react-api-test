3623813f964945171c4807543492a523a83e17b0
izumi-takeshima <o.inzauhmci@gmail.com>
2021-04-18 22:30:14 +1000

    Updates

---
+++ precache-manifest.921f79825d1a45d6384d51f7cc08a4ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7938736e12475fe1083f85a65b4914cd",
+    "url": "/react-api-test/index.html"
+  },
+  {
+    "revision": "76c51184ddefc4e3c498",
+    "url": "/react-api-test/static/css/main.fa95dedb.chunk.css"
+  },
+  {
+    "revision": "2d19631dba197ad7891e",
+    "url": "/react-api-test/static/js/2.e1627e68.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-api-test/static/js/2.e1627e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c51184ddefc4e3c498",
+    "url": "/react-api-test/static/js/main.fa45fc0b.chunk.js"
+  },
+  {
+    "revision": "9075c735a60d404a7e56",
+    "url": "/react-api-test/static/js/runtime-main.7c0f0b5e.js"
+  }
+]);
